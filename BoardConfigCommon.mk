24b07378a8c441e3cd0cc1f7df46130544461602
akash07k <akash.galaxy07@gmail.com>
2023-02-12 11:42:37 +0530

    sm8250-common: Build with LTO
    Signed-off-by: akash07k <akash.galaxy07@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,6 +126,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/munch
 TARGET_KERNEL_ARCH := arm64
 KERNEL_LD := LD=ld.lld
+TARGET_BUILD_WITH_LTO := true
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := latest
 TARGET_KERNEL_ADDITIONAL_FLAGS += AR=$(shell pwd)/prebuilts/clang/host/linux-x86/clang-latest/bin/llvm-ar
